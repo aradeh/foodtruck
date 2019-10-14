42d492e82410582c6a56bd17ec829d6f872e4c02
aradeh <harsh.arde@gmail.com>
2019-10-14 00:36:33 -0400

    sorting according to applicant

--- app.js
+++ app.js
@@ -6,19 +6,25 @@ http.createServer((req,res) => {
     if(req.url === "/getfoodtruck"){
         apiCallRequest.callApi(function(response){
             let truckObj = [];
-            console.log(currentDayTime.nowDayTime);
-            //console.log(currentDayTime.day);
+            //console.log(currentDayTime.nowDayTime);
             for(i in response){
                 if(response[i].dayofweekstr == currentDayTime.nowDayTime.day && currentDayTime.nowDayTime.time >= response[i].start24 &&  currentDayTime.nowDayTime.time <=response[i].end24){
                     truckObj.push(response[i]);
                 }
             }
-            //res.write(JSON.stringify(response.dayofweekstr == "Monday"));
-            res.write(JSON.stringify(truckObj));
+
+            truckObj.sort(function (a,b){ 
+                return a.applicant > b.applicant;
+            });
+
+            res.write(JSON.stringify(truckObj,null,'\t'));
             //res.write(response);
             res.end();
         });
     }
 }).listen(3000);
 
+
+
 console.log("server running on port 3000.");
+//console.log(truckObj);
