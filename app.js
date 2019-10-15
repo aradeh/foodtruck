b94a3f527f303b45f5115fabb73ebf7a02abb3a6
aradeh <harsh.arde@gmail.com>
2019-10-14 22:58:34 -0400

    sort

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 const apiCallRequest = require('./get-api-data');
 const currentDayTime = require('./get-current-daytime');
 const http = require('http');
+var pagination = require('pagination');
+
 let truckObj = [];
 
 http.createServer((req,res) => {
     if(req.url === "/getfoodtruck"){
         apiCallRequest.callApi(function(response){
-            //let truckObj = [];
             console.log(currentDayTime.nowDayTime);
             for(i in response){
                 if(response[i].dayofweekstr == currentDayTime.nowDayTime.day && currentDayTime.nowDayTime.time >= response[i].start24 &&  currentDayTime.nowDayTime.time <=response[i].end24){
-                    //truckObj.push(response[i]);
                     addObj(response[i].applicant, response[i].location);
                 }
             }
-
+            var paginator = pagination.create('search', {prelink:'/', current: 1, rowsPerPage: 10, totalResult: 100});
+            console.log("PAGE");
+            console.log(paginator.render());
             //sorting alphabetically with applicant name field
-            truckObj.sort(function (a,b){ 
-                return a.name > b.name;
-            });
+           // truckObj.sort((a,b) => a.name.localeCompare(b.name));
 
-            res.write(JSON.stringify(truckObj,null,'\t'));
+            res.write(
+                JSON.stringify(truckObj,null,'\t')
+            );
             console.log("------------------------------------------------------------------------------------------------------------------------------------------------------------");
-            console.log(JSON.stringify(truckObj,null,'\t'));
+            //console.log(JSON.stringify(truckObj,null,'\t'));
             res.end();
         });
     }
